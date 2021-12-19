cd23f75e6fa999ca185bd5c3e3177945f633d339
akiftasbas <32tasbasakif32@gmail.com>
2021-12-19 20:11:36 +0100

    Second day of my project

---
+++ src/test/java/subject01/Test02.java
@@ -0,0 +1,10 @@
+package subject01;
+
+import java.sql.SQLOutput;
+
+public class Test02 {
+
+    public static void main(String[] args) {
+        System.out.println("We have maid some changes at our projeckt");
+    }
+}
