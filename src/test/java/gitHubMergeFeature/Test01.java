0f995a37e0bc6e82e295ec43abac21a408f7bf54
akiftasbas <32tasbasakif32@gmail.com>
2021-12-19 20:51:31 +0100

    New GitHub Branch Commit

---
+++ src/test/java/gitHubMergeFeature/Test01.java
@@ -0,0 +1,7 @@
+package gitHubMergeFeature;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("I made this changes localy, but i will merge them at the GitHub");
+    }
+}
